4906ece4eda9d7d13a442b69db5abd73eda89d5b
magesh <magesh@star-systems.in>
2025-07-08 00:52:07 +0530

    adding new codes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
